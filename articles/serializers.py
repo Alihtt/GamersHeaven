5f3b7fe743aa15604df0ea7673484cab52667676
Alihtt <aliht.workspace@gmail.com>
2024-03-20 04:59:23 +0330

    add url and author name in serializers.py

--- articles/serializers.py
+++ articles/serializers.py
@@ -1,17 +1,17 @@
 from rest_framework import serializers
-from .models import Article, Category
-
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = '__all__'
+from .models import Article
 
 
 class ArticleSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField()
+    url = serializers.SerializerMethodField()
+
     class Meta:
         model = Article
         fields = '__all__'
+
+    def get_url(self, obj):
+        return obj.get_absolute_url()
 
     def validate_author(self, data):
         if data == self.context['request'].user:
